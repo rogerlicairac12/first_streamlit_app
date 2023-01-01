89ee4ab7506d65716dd4fdc2e12977f726094b5e
snow flake Roger <121760724+rogerlicairac12@users.noreply.github.com>
2023-01-01 12:27:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free Range-Egg')
 
