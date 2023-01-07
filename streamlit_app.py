7664cd08026d3f34d14dd81298ed23c47392960d
snow flake Roger <121760724+rogerlicairac12@users.noreply.github.com>
2023-01-07 16:55:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ def insert_row_snowflake(new_fruit):
             my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
             return "Thanks for adding " + new_fruit
 
-streamlit.stop()
+
       
 add_my_fruit = streamlit.text_input('What fruit would you like to add?') 
 if streamlit.button('Add a Fruit to the List'):
@@ -91,5 +91,5 @@ if streamlit.button('Add a Fruit to the List'):
       back_from_function = insert_row_snowflake(add_my_first)
       streamlit.text(back_from_function)
 
-
+streamlit.stop()
 
