70c5b8ffebe05adaafe5b228f1afc3c7e3eddc74
snow flake Roger <121760724+rogerlicairac12@users.noreply.github.com>
2023-01-07 18:58:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,10 +87,10 @@ def d(n):
 
 
       
-add_my_fruit = streamlit.text_input('What fruit would you like to add?') 
+a = streamlit.text_input('What fruit would you like to add?') 
 #if streamlit.button('Add a Fruit to the List'):
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-back_from_function = d(add_my_first)
+back_from_function = d(a)
 streamlit.stop()
 
 streamlit.text(back_from_function)
