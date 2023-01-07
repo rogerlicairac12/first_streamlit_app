ddf3ca2c1a428a208d7c1b013b709368b4d36506
snow flake Roger <121760724+rogerlicairac12@users.noreply.github.com>
2023-01-07 14:52:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
