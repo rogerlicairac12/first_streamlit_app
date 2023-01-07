b35ee9c4ff1e93817fea7f1df2e7e9ec44c2cd7c
snow flake Roger <121760724+rogerlicairac12@users.noreply.github.com>
2023-01-07 16:31:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,6 @@ if streamlit.button('Get Fruit Load List'):
       my_data_rows = get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
 
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
@@ -90,4 +89,5 @@ if streamlit.button('Add a Fruit to the List'):
       back_from_function = insert_row_snowflake(add_my_first)
       streamlit.text(back_from_function)
 
+streamlit.stop()
 
