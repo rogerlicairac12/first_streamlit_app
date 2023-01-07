f1d82c9bb60f5f33043e52cb1266ca07650ac1df
snow flake Roger <121760724+rogerlicairac12@users.noreply.github.com>
2023-01-07 13:06:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,4 +57,4 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?') 
 
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thank you for adding ', add_my_fruit)
