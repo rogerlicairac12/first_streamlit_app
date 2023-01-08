8ea1d91cd3162316d1b3a1c7e7dbed94fa2a7ae6
snow flake Roger <121760724+rogerlicairac12@users.noreply.github.com>
2023-01-07 20:38:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,10 +83,6 @@ def insert_row_snowflake(new_fruit):
             my_cur.execute("insert into fruit_load_list values ('from')")
             return "thanks for adding " + new_fruit
 
-#def d(new_fruit):
-#      with my_cnx.cursor() as my_cur:
-#            my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-#            return "Thanks for adding" + new_fruit
 
 
 add_my_fruit = streamlit.text_input('what fruit would you like to add?')
